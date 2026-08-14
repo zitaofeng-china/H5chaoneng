18aaff65d3b256c0572a065b4f9afac7b0544db5
zitaofeng988 <zitaofeng988@gmail.com>
2026-08-14 22:54:13 +0800

    feat: 按原型调整首页区块展示顺序

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,8 +1,8 @@
 <template>
   <EnergyRental />
-  <WhyChooseUs />
-  <HowItWorks />
   <FeeDescription />
+  <HowItWorks />
+  <WhyChooseUs />
   <FaqSection />
   <HelpSection />
 </template>
@@ -12,9 +12,9 @@ import { defineAsyncComponent } from 'vue'
 import EnergyRental from './components/EnergyRental.vue'
 
 // 首屏主转化区同步加载；下方营销区块异步分包
-const WhyChooseUs = defineAsyncComponent(() => import('./components/WhyChooseUs.vue'))
-const HowItWorks = defineAsyncComponent(() => import('./components/HowItWorks.vue'))
 const FeeDescription = defineAsyncComponent(() => import('./components/FeeDescription.vue'))
+const HowItWorks = defineAsyncComponent(() => import('./components/HowItWorks.vue'))
+const WhyChooseUs = defineAsyncComponent(() => import('./components/WhyChooseUs.vue'))
 const FaqSection = defineAsyncComponent(() => import('./components/FaqSection.vue'))
 const HelpSection = defineAsyncComponent(() => import('./components/HelpSection.vue'))
 
